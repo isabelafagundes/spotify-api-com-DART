e0e08ec51b30a508ee6b85b78a7613dc7c593eef
Isabela  Fagundes <104397121+isabelafagundes@users.noreply.github.com>
2024-01-14 10:16:52 -0300

    Update environment.dart

--- lib/environment.dart
+++ lib/environment.dart
@@ -1,7 +1,7 @@
 class Environment {
 
-  static const CLIENT_ID = 'd70f80db57ea4aa797272ec9dc44414d';
-  static const  CLIENT_SECRET = '4532da86861047bf86c8722a5161b364';
+  static const CLIENT_ID = '';
+  static const  CLIENT_SECRET = '';
   static const REDIRECT_URI = 'http://localhost:8080/callback';
   static const FRONT_URI = 'http://localhost:8089/pallete';
 
